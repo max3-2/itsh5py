d4a2217141235141a18276207f37757001f0288f
Max Elfner <maximilian.elfner@kit.edu>
2021-02-04 13:11:17 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -3,31 +3,30 @@
 import sys
 import glob
 import setuptools
-from podbox import __version__, __uiname__
+from itsh5py import __version__
 
 # ======================================
 # Minimal Python version sanity check
 # ======================================
 V = sys.version_info
 if (V[0] >= 3 and V[:2] < (3, 6)):
-    ERR = "ERROR: podbox requires Python 3.6 and above."
+    ERR = "ERROR: itsh5py requires Python 3.6 and above."
     print(ERR, file=sys.stderr)
     sys.exit(1)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-imageFiles = ['/'.join(g.split('/')[1:]) for g in glob.glob('podbox/images/*')]
 
 setuptools.setup(
-    name=__uiname__,
+    name='itsh5py',
     version=__version__,
     author="M. Elfner, ITS, KIT",
     author_email="maximilian.elfner@kit.edu",
-    description="The ITS podbox",
+    description="ITS h5 support",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git.scc.kit.edu/itsheat/podbox",
+    url="https://git.scc.kit.edu/its-all/academics/itsh5py",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -35,22 +34,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    entry_points={
-        'console_scripts': ['podbox = podbox.main:runMainUI',
-                            'davisstacker = podbox.main:runStacker',]
-        },
-    package_data={
-        'podbox':imageFiles
-        },
     install_requires=[
         'numpy',
-        'matplotlib',
-        'PyQt5',
-        'qtpy',
-        'seaborn',
-        'qdarkstyle',
-        'tifffile',
-        'scikit-image',
+        'yaml',
         'h5py',
     ],
 )
