cc39a7be4aae2df7865e299fa55b131b26895af6
M. Elfner <maximilian.elfner@kit.edu>
2021-02-24 15:49:36 +0100

    Updates to hdf tuple save format
    naming style for long tuples, Regression bug in np.str save support

--- itsh5py/hdfSupport.py
+++ itsh5py/hdfSupport.py
@@ -169,9 +169,9 @@ def load(hdf, lazy=False, unpacker=unpack_dataset):
             # away, anything else is way to complicated
             if TYPEID in v.attrs:
                 if v.attrs[TYPEID] == 'tuple':
-                    dl.append(_recurseIterData(v, 'tuple'))
+                    dl.append(_recurseIterData(v, True))
                 elif v.attrs[TYPEID] == 'list':
-                    dl.append(_recurseIterData(v, 'list'))
+                    dl.append(_recurseIterData(v))
                 else:
                     dl.append(unpacker(v))
             else:
@@ -306,24 +306,26 @@ def pack_dataset(hdfobject, key, value, compress):
                 group.create_dataset(
                     name=name, data=array)
 
-
     def _iterateIterData(hdfobject, key, value, typeID):
             ds = hdfobject.create_group(key)
+            elementsOrder = int(np.floor(np.log10(len(value))) + 1)
+            fmt = 'i_{:0' + str(elementsOrder) + 'd}'
             for i, v in enumerate(value):
                 if isinstance(v, tuple):
-                    _iterateIterData(ds, 'i_' + str(i), v, "tuple")
+                    _iterateIterData(ds, fmt.format(i), v, "tuple")
                 elif isinstance(v, list):
                     # check for mixed type, if yes, dump to group as tuple
                     if not all([type(v) == type(value[0]) for v in value]):
                         _iterateIterData(hdfobject, key, value, "list")
+                    else:
+                        _iterateIterData(ds, fmt.format(i), v, "list")
                 else:
                     if isinstance(v, np.ndarray):
-                        _dumpArray('i_' + str(i), v, ds, compress)
-
+                        _dumpArray(fmt.format(i), v, ds, compress)
                     else:
                         if isinstance(v, np.str_) or isinstance(v, np.str):
-                            value = str(value)
-                        ds.create_dataset(name='i_' + str(i), data=v)
+                            v = str(v)
+                        ds.create_dataset(name=fmt.format(i), data=v)
 
             ds.attrs.create(
                 name=TYPEID,
