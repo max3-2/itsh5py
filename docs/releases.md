0256cf2a270797e9880275413596363fe7641fca
M. Elfner <maximilian.elfner@kit.edu>
2021-08-09 12:31:21 +0200

    Metadata, Version Bump 0.7.0

--- docs/releases.md
+++ docs/releases.md
@@ -1,5 +1,5 @@
 # Releases
 
 ## 0.6.0
-* [Published](https://pypi.org/project/itsh5py/0.6.0) on 2021-August
+* [Published](https://pypi.org/project/itsh5py/0.6.0) on 2021-08-09
 * Initial Release on PyPI
--- itsh5py/__init__.py
+++ itsh5py/__init__.py
@@ -2,8 +2,8 @@
 """
 # Metadata
 __title__ = 'itsh5py'
-__version__ = '0.6.0'
-__date__ = '2021–07–30'
+__version__ = '0.7.0'
+__date__ = '2021–08–09'
 __author__ = 'Max Elfner'
 __copyright__ = 'Max Elfner, ITS, KIT https://www.its.kit.edu'
 __license__ = 'MIT'
