3d8a3524d41350546a73c2628026f151d65d2e75
M. Elfner <maximilian.elfner@kit.edu>
2021-08-09 11:28:27 +0200

    Version set up

--- itsh5py/__init__.py
+++ itsh5py/__init__.py
@@ -2,8 +2,8 @@
 """
 # Metadata
 __title__ = 'itsh5py'
-__version__ = '0.6.3'  # Remove on release, set to 0.6.0.
-__date__ = '2021–06–15'
+__version__ = '0.6.0'
+__date__ = '2021–07–30'
 __author__ = 'Max Elfner'
 __copyright__ = 'Max Elfner, ITS, KIT https://www.its.kit.edu'
 __license__ = 'MIT'
